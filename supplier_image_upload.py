9d127a0c2d936c9e02f41ceaea0036f0013d8370
Patrick Quinn <quinnpatj@gmail.com>
2022-02-12 10:27:27 -0600

    Completed "Uploading images to web server" section

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
 import requests
+import os
+
+def main():
+    url = "http://localhost/upload/"
+    path = "supplier-data/images/"
+
+    for file in os.listdir(path):
+        if file.endswith(".jpeg"):
+            with open(path + file, 'rb') as im:
+                r = requests.post(url, files={'file': im})
+
+if __name__ == '__main__':
+    main()
