ebb09391139e4e20ff81beecc4f9d539aaae9ee1
Patrick Quinn <quinnpatj@gmail.com>
2022-02-12 14:11:06 -0600

    Completed "Generate PDF and send through email"

--- report_email.py
+++ report_email.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+
+import os
+import datetime
+import reports
+import emails
+
+date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+def process_text(path):
+    processed = ""
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+            f = open(path + file)
+            val = f.readlines()
+            name = val[0]
+            weight = val[1]
+            processed += "name: " + name + "<br/>" + "weight: " + weight + "<br/>"
+    return processed
+
+
+if __name__ == "__main__":
+    path = 'supplier-data/descriptions/'
+    attach = "/tmp/processed.pdf"
+    title = "Process Updated on " + date
+    body = process_text(path)
+    reports.generate_report(attach, title, body)
+
+    sender = "automation@example.com"
+    recipient = "{}@example.com".format(os.environ["USER"])
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    message = emails.generate_email(sender, recipient, subject, body, attach)
+    emails.send_email(message)
--- reports.py
+++ reports.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate_report(attach, title, body):
+    report = SimpleDocTemplate(attach)
+    styles = getSampleStyleSheet
+    report_title = Paragraph(title, styles["h1"])
+    report_body = Paragraph(body, styles["BodyText"])
+    br = Spacer(1,20)
+    report.build([report_title, br, report_body, br])
