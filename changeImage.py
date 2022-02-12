39ccc14d8986e21dc4047c50f04fb6277463d480
Patrick Quinn <quinnpatj@gmail.com>
2022-02-11 21:03:36 -0600

    Completed "Working with supplier images" section

--- changeImage.py
+++ changeImage.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+def main():
+    path = 'supplier-data/images/'
+
+    for file in os.listdir(path):
+        if 'tiff' in file:
+            imPath = path + file
+            outFile = file.split('.')[0] + ".jpeg"
+            outPath = path + outFile
+            im = Image.open(imPath)
+            im.convert('RGB').resize((600,400)).save(outPath, "JPEG")
+
+if __name__ == '__main__':
+    main()
